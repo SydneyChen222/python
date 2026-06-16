a614c499beb533d5168d59476bf61dc64347f44a
Xinyi (Sydney) Chen <62270429+SydneyChen222@users.noreply.github.com>
2026-06-15 23:12:48 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -11,13 +11,14 @@ df=sales.groupby(['product_id','month','region'],as_index=False).agg(revenue = (
 df['month'] = pd.to_datetime(df['month']) 
 df = df.set_index('month').sort_index() 
 df['rolling'] = df.groupby(['product_id'])['revenue'].transform(lambda x: x.rolling("3M").mean())
+#this 3M will force the rolling calculation based on 3 consecutive month and if there is a month NaN it will still count it in like: (NaN+NonNANmonth+NoneNANmonth)/ 2 (since there are 2valid month)
 df = df.reset_index() # brings 'month' back as a regular column
 # or 
 df = df.sort_values(['product_id', 'month'])
 df['rolling'] = (
     df.groupby('product_id')['revenue']
     .transform(lambda x: x.rolling(3, min_periods=1).mean())
-)
+) # this will calculate as long as there is 1 month data not NAN and if there is a skipped month it will not detect and will just use next avaliable month
 
 df['varian'] = (df['rolling'] - df['revenue'])/df['revenue'].replace(0,np.nan) 
 df['flag'] = df['varian'].apply(lambda x: "Anomaly" if x > 0.2 else ("Normal" if x<=0.2 else "UNKNOWN"))
